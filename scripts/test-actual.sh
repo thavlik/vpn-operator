923d0b463e14d963b894dd95256dc01c17f14c2a
Tom Havlik <thavlik@protonmail.com>
2023-03-14 00:19:56 -0500

    ...

--- scripts/test-actual.sh
+++ scripts/test-actual.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 cd $(dirname $0)/..
+# Setting these variables uses a real VPN service for testing.
 export SECRET_NAME=actual-vpn-cred
 export SECRET_NAMESPACE=vpn
 cargo test -j 10 basic

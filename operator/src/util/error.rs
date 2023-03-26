7cc542154af69b0510f22a1d198a96c3f85bcf07
Tom Havlik <thavlik@protonmail.com>
2023-03-25 22:44:53 -0500

    ...

--- operator/src/util/error.rs
+++ operator/src/util/error.rs
@@ -1,18 +1,14 @@
-/// All errors possible to occur during reconciliation
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
-    /// Any error originating from the `kube-rs` crate
     #[error("Kubernetes reported error: {source}")]
     KubeError {
         #[from]
         source: kube::Error,
     },
 
-    /// Usually an error in a Mask, Provider, or a Provider's secret.
     #[error("Invalid user input: {0}")]
     UserInputError(String),
 
-    /// Chrono date parsing error
     #[error("Failed to parse DateTime: {source}")]
     ChronoError {
         #[from]
